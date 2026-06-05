7ade99f402ec45b8d48ceda73c3a7d290f061244
Mauriculus <mauroscan20@gmail.com>
2026-06-04 21:17:48 -0300

    versão notebook desatualizada

--- server/models/User.js
+++ server/models/User.js
@@ -3,13 +3,28 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, maxlength: 20 },
 
+  lastUsernameChange: { type: Date, default: null },
+
+  usernameNormalized: { type: String, required: true, unique: true },
+
   email: { type: String, required: true, unique: true },
 
-  password: { type: String, required: true },
+  password: { 
+    type: String,
+    required: true,
+    match: [/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$/, 'Senha não atende aos requisitos']},
+
+  profilePicture: { type: String, default: '' },
 
   type: { type: String, enum: ['usuario', 'admin'], default: 'usuario' },
 
-  banido: { type: Boolean, default: false }
+  following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+
+  followers: { type: Number, default: 0 },
+
+  banned: { type: Boolean, default: false },
+
+  isVerified: { type: Boolean, default: false },x
 
 }, { timestamps: true });
 
